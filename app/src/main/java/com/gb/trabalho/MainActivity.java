bf1324b18dc341acc3dea450b01c92195b6c4bcf
TKzErMaK <82242363+TKzErMaK@users.noreply.github.com>
2025-05-01 18:33:12 -0300

    v0.0.3
    Adicionado Lista de Metas

--- app/src/main/java/com/gb/trabalho/MainActivity.java
+++ app/src/main/java/com/gb/trabalho/MainActivity.java
@@ -1,6 +1,7 @@
 package com.gb.trabalho;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,11 +15,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.main_activity);
+        setContentView(R.layout.top_bar);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
     }
 }
